a4923afd2378106bc11d8f82f833467ca337249a
SriMidhinesh <midhinesh.92@gmail.com>
2024-12-27 13:36:51 +0530

    Added new code
    script.js - Conversion of Bin2Dec using natural log instead of Arrays.

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const inputField = document.getElementById("input");
 const warning = document.getElementById("warning");
-let result = document.getElementById("result");
+const result = document.getElementById("result");
 
+document.getElementById("button").addEventListener("click", () => {
+    warning.style.display = "none"; 
+    let inputVal = inputField.value.trim(); 
+    let position = 0; 
+    let sum = 0; 
 
-document.getElementById("button").addEventListener('click', () => {
-    const inputVal = inputField.value;
-    // console.log(typeof inputValue);
-    let sum = 0;
-    let n = inputVal.length;
-    for(let i = 0; i<n; i++) {
-
-        if (inputVal[i] == "0" || inputVal[i] == "1") {
-            warning.style.display = "none";
-            let intValue = parseInt(inputVal[i]);
-            sum += intValue*(2**(n-i-1));
-        }
-        else {
-            warning.style.display = "block";
-            sum = 0
-            result.value = none;
-            break;
-        }
+    if (inputVal.length == 0) {
+        result.value = "";
+        return;
     }
-    // console.log(sum);
+
+    for (let i = inputVal.length - 1; i >= 0; i--) {
+        const digit = inputVal[i];
+        if (digit === "0") {
+            position++;
+            continue;
+        } else if (digit === "1") {
+            sum += Math.ceil(Math.exp(position * Math.log(2)));
+        } else {
+            warning.style.display = "block";
+            result.value = "";
+            return;
+        }
+        position++;
+    }
     result.value = sum;
-})
+});
